a2ba764ea40175a23524216d1c83f5d6a85bef07
Kai-R <555kaireffert@gmail.com>
2022-06-25 00:53:06 +0200

    'main'

--- underlying_functions.py
+++ underlying_functions.py
@@ -2,21 +2,18 @@ import pandas as pd
 import sklearn 
 import numpy as np
 
-class Functions:
+def free():
     print(4)
-    def __init__(self):
-        pass
-    # this method adds a intercept to a given dataframe
+# this method adds a intercept to a given dataframe
+def intercept_add(df):
+    df['Intercept'] = 1.0
+    return df
 
-    def intercept_add(df):
-        df['Intercept'] = 1.0
-        return df
-
-    # this method performs linear regression of a given X and Y
-    # then the RSS and R-squared are computed with the sklearn module
-    # def lrg(X, Y):
-        # model = linear_model.LinearRegression()  # fit_intercept=True
-        #model.fit(X, Y)
-        #RSS = mean_squared_error(Y, model.predict(X))*len(Y)
-        #R_squared = model.score(X, Y)
-        # return RSS, R_squared
+# this method performs linear regression of a given X and Y
+# then the RSS and R-squared are computed with the sklearn module
+# def lrg(X, Y):
+    # model = linear_model.LinearRegression()  # fit_intercept=True
+    #model.fit(X, Y)
+    #RSS = mean_squared_error(Y, model.predict(X))*len(Y)
+    #R_squared = model.score(X, Y)
+    # return RSS, R_squared
