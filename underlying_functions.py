5ccadf907d480fce3173d4be038edd747d75c130
Kai-R <555kaireffert@gmail.com>
2022-06-25 16:08:34 +0200

    'updated modularity and requirements'

--- underlying_functions.py
+++ underlying_functions.py
@@ -2,18 +2,14 @@ import pandas as pd
 import sklearn 
 import numpy as np
 
-def free():
-    print(4)
-# this method adds a intercept to a given dataframe
-def intercept_add(df):
-    df['Intercept'] = 1.0
-    return df
+s = "If Comrade Napoleon says it, it must be right."
+a = [100, 200, 300]
 
-# this method performs linear regression of a given X and Y
-# then the RSS and R-squared are computed with the sklearn module
-# def lrg(X, Y):
-    # model = linear_model.LinearRegression()  # fit_intercept=True
-    #model.fit(X, Y)
-    #RSS = mean_squared_error(Y, model.predict(X))*len(Y)
-    #R_squared = model.score(X, Y)
-    # return RSS, R_squared
+def intercept_add(df):
+    df['Intercept']=1.0
+    return df
+    
+def foo(arg):
+    print(f'arg = {arg}')
+
+
